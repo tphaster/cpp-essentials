cc0e939ab9946f7b612ebf59fc3a52d2c46936a2
Tomasz Pieczerak <tphaster@gmail.com>
2020-02-07 16:37:14 +0100

    Adjusted formatting, more elaborated comments

--- idioms/copy_and_swap.h
+++ idioms/copy_and_swap.h
@@ -1,8 +1,10 @@
 /**
  *        File:  copy_and_swap.h
- * Description:  The copy-and-swap idiom example.
+ * Description:  The copy-and-swap idiom example. It avoids code duplication
+ *               and provides a strong exception guarantee in the assignment
+ *               operator and move constructor implementations.
  *
- *      Author:  Tomasz Pieczerak (tph) <tphaster AT gmail.com>
+ *      Author:  Tomasz Pieczerak <tphaster AT gmail.com>
  */
 
 #include <algorithm>    // std::copy
@@ -11,56 +13,57 @@
 class DumbArray
 {
 public:
-    /* (default) constructor */
-    DumbArray (std::size_t size = 0)
-        : mSize(size), mArray(mSize ? new int[mSize]() : nullptr)
-    {
-    }
+	/* (default) constructor */
+	DumbArray(std::size_t size = 0)
+		: mSize(size), mArray(mSize ? new int[mSize]() : nullptr)
+	{
+	}
 
-    /* copy-constructor */
-    DumbArray (const DumbArray& other)
-        : mSize(other.mSize), mArray(mSize ? new int[mSize] : nullptr)
-    {
-        // note: this one is non-throwing, only because of the data types
-        // used, in general case exceptions will need some attention
-        std::copy(other.mArray, other.mArray + mSize, mArray);
-    }
+	/* copy-constructor */
+	DumbArray(const DumbArray& other)
+		: mSize(other.mSize), mArray(mSize ? new int[mSize] : nullptr)
+	{
+		// note: this one is non-throwing only because of the data types
+		// used, in general case exceptions will need some attention
+		std::copy(other.mArray, other.mArray + mSize, mArray);
+	}
 
-    /* assignment operator */
-    DumbArray& operator= (DumbArray other) // let the compiler make a copy
-    {
-        // there is no need for a self-assignment check
-        swap(*this, other);
+	/* assignment operator */
+	DumbArray& operator=(DumbArray other)   // let the compiler make a copy
+	{
+		// there is no need for a self-assignment check
+		swap(*this, other);
 
-        return *this;
-    }
+		return *this;
+	}
 
-    /* move constructor */
-    DumbArray (DumbArray&& other)
-        : DumbArray() // initialize via default constructor, C++11 only
-    {
-        swap(*this, other);
-    }
+	/* move constructor */
+	DumbArray(DumbArray&& other)
+		: DumbArray()   // initialize via default constructor, C++11 only
+	{
+		swap(*this, other);
+	}
 
-    /* destructor */
-    ~DumbArray()
-    {
-        delete [] mArray;
-    }
+	/* destructor */
+	~DumbArray()
+	{
+		delete [] mArray;
+	}
 
-    /* swap function */
-    friend void swap (DumbArray& first, DumbArray& second) // nothrow
-    {
-        // enable ADL (not necessary in our case, but good practice)
-        using std::swap;
+	/* swap() - public friend function */
+	friend void swap(DumbArray& first, DumbArray& second) // nothrow
+	{
+		// enable ADL (not necessary in our case, but good practice and also
+		// an established idiom)
+		using std::swap;
 
-        // by swapping the members of two objects,
-        // the two objects are effectively swapped
-        swap(first.mSize, second.mSize);
-        swap(first.mArray, second.mArray);
-    }
+		// by swapping the members of two objects,
+		// the two objects are effectively swapped
+		swap(first.mSize, second.mSize);
+		swap(first.mArray, second.mArray);
+	}
 
 private:
-    std::size_t mSize;
-    int* mArray;
+	std::size_t mSize;
+	int* mArray;
 };
--- idioms/covariant_return.cpp
+++ idioms/covariant_return.cpp
@@ -1,6 +1,8 @@
 /**
  *        File:  covariant_return.cpp
- * Description:  The covariant return types idiom example.
+ * Description:  The covariant return types idiom example. Avoid unnecessary
+ *               casting of the derived value returned from an overridden
+ *               method in a derived class.
  *
  *      Author:  Tomasz Pieczerak (tph) <tphaster AT gmail.com>
  */
@@ -8,27 +10,31 @@
 class Base
 {
 public:
-    virtual Base* clone() const
-    {
-        return new Base(*this);
-    }
+	virtual Base* clone() const
+	{
+		return new Base(*this);
+	}
 };
 
 class Derived : public Base
 {
 public:
-    virtual Derived* clone() const override
-    {
-        return new Derived(*this);
-    }
+	/* override base class method - return Derived pointer, not Base pointer */
+	virtual Derived* clone() const override
+	{
+		return new Derived(*this);
+	}
 };
 
 int main (void)
 {
-    Derived *d1 = new Derived();
-    Derived *d2 = d1->clone();
+	Derived *d1 = new Derived();
+	// now we can avoid this:
+	//   Base *b = d1->clone();
+	//   Derived *d2 = dynamic_cast<Derived*>(b);
+	Derived *d2 = d1->clone();
 
-    if (d2 != nullptr) {
-        // use d2 here
-    }
+	if (d2 != nullptr) {
+		// use d2 here
+	}
 }
